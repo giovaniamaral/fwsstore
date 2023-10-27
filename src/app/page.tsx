b2bb36f95c80ff39f5f7439a7346826e31f12da9
Giovani Amaral <94939971+giovaniamaral@users.noreply.github.com>
2023-10-27 16:23:36 -0300

    page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-
 export default function Home() {
-  const { data } = useSession();
-  return <div>{data?.user?.name}</div>;
+  return <div></div>;
 }
