a97ad29da452f93868a8bfe7c9af79f15592ed11
Giovani Amaral <94939971+giovaniamaral@users.noreply.github.com>
2023-10-30 11:36:56 -0300

    feat : add second image home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -25,8 +25,17 @@ export default async function Home() {
         <Categories />
       </div>
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner.home02.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto em mouses!"
+        className="mt-8 h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   );
 }
